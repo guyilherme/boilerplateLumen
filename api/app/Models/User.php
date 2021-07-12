63219407e3391c680f7a50347de6ee3ed3db8e83
Guilherme Macêdo da Cruz <eu@guilhermecruz.dev>
2021-07-11 21:03:11 -0300

    feat: :sparkles: Adicionado o passport ao projeto
    Adicionado o pacote dusterio passport para autentitação

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Lumen\Auth\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use Authenticatable, Authorizable, HasFactory, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
